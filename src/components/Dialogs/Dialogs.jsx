ac9c81e07b7a677115659265809d165f1f6bad90
frontvin <bvs0618@gmail.com>
2019-06-21 19:20:33 +0300

    ended 24

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,30 +17,35 @@ const Message = (props) => {
 };
 
 export const Dialogs = (props) => {
+
+  let dialogsData = [
+    { id: 1, name: "Vika" },
+    { id: 2, name: "Dimych" },
+    { id: 3, name: "Andrey" },
+    { id: 4, name: "Sveta" },
+    { id: 5, name: "Sacha" },
+    { id: 6, name: "Viktor" },
+   ];
+
+  let messagesData = [
+    { id: 1, message: "Vika" },
+    { id: 2, message: "How is your deals?" },
+    { id: 3, message: "Yo!" },
+    { id: 4, message: "Vika" },
+    { id: 5, message: "Vika" },
+    { id: 6, message: "Vika" },
+  ];
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <DialogItem name="Dimych" id="1"/>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/2">Andrey</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/3">Sveta</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/4">Sacha</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/5">Viktor</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/5">Dimych</NavLink>
-        </div>
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[0].id} />
       </div>
       <div className={s.messages}>
-        <Message message="Hi" />
-        <div className={s.message}>How is your deals?</div>
-        <div className={s.message}>Yo!</div>
+        <Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />
+        <Message message={messagesData[2].message} />
       </div>
     </div>
   )
